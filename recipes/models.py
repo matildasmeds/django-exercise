1c1dc0050fe0aae3b33d1022e9e313c3c9c03834
Matilda <matilda.smeds@gmail.com>
2020-02-10 11:56:29 +0100

    Models & migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
