560ac051525aa0e850306f2e8a45168db3b1ab3c
Matilda <matilda.smeds@gmail.com>
2020-02-10 14:45:38 +0100

    First test... note, change to FactoryBoy

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+import json
 
-# Create your tests here.
+
+class RecipesListTests(TestCase):
+    def test_recipes_list(self):
+        """Test GET /api/recipes"""
+        url = '/api/recipes'
+        res = self.client.get(url, format='json')
+
+        expected = [
+            {
+                "id": 1,
+                "name": "Avocado Pasta",
+                "description": "Easy and fun",
+                "ingredients": [{"name": "Avocado"}, {"name": "Pasta"}],
+            }
+        ]
+        self.assertEquals(json.loads(res.content), expected)
+        self.assertEquals(res.status_code, 200)
