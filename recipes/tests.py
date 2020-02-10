a1be848db8fed8edaafd983e06a6b72a74d34045
Matilda <matilda.smeds@gmail.com>
2020-02-10 14:55:27 +0100

    Failing test for GET recipe

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,20 +1,30 @@
 from django.test import TestCase
 import json
 
+RECIPE_SERIALIZATION = {
+    "id": 1,
+    "name": "Avocado Pasta",
+    "description": "Easy and fun",
+    "ingredients": [{"name": "Avocado"}, {"name": "Pasta"}],
+}
+
 
 class RecipesListTests(TestCase):
-    def test_recipes_list(self):
-        """Test GET /api/recipes"""
-        url = '/api/recipes'
-        res = self.client.get(url, format='json')
 
-        expected = [
-            {
-                "id": 1,
-                "name": "Avocado Pasta",
-                "description": "Easy and fun",
-                "ingredients": [{"name": "Avocado"}, {"name": "Pasta"}],
-            }
-        ]
+    def test_get_recipes(self):
+        """Test GET /api/recipes"""
+        url = "/api/recipes"
+        res = self.client.get(url, format="json")
+
+        expected = [RECIPE_SERIALIZATION]
+        self.assertEquals(json.loads(res.content), expected)
+        self.assertEquals(res.status_code, 200)
+
+    def test_get_recipe(self):
+        """Test GET /api/recipes/:id"""
+        url = "/api/recipes/1"
+        res = self.client.get(url, format="json")
+
+        expected = RECIPE_SERIALIZATION
         self.assertEquals(json.loads(res.content), expected)
         self.assertEquals(res.status_code, 200)
