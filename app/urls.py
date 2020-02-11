cf340117e76b31c1c29b04a616343216506fe687
Matilda <matilda.smeds@gmail.com>
2020-02-11 10:01:00 +0100

    Trying to use ModelSetView, tests fail

--- app/urls.py
+++ app/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+from recipes.views import RecipeViewSet
+
+api_router = DefaultRouter()
+api_router.register(r'api/recipes', RecipeViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('recipes.urls'))
-]
+] + api_router.urls
--- recipes/views.py
+++ recipes/views.py
@@ -3,36 +3,16 @@ from django.views.decorators.csrf import csrf_exempt
 # from rest_framework.parsers import JSONParser
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer
-from django.core.exceptions import ObjectDoesNotExist
-from rest_framework.parsers import JSONParser
-from rest_framework.decorators import api_view, parser_classes
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.renderers import JSONRenderer
 
 
-@csrf_exempt
-@api_view(['GET', 'POST'])
-@parser_classes([JSONParser])
-def recipe_list(request):
-    """
-    List all recipes, or create new recipe
-    """
-    if request.method == 'GET':
-        recipes = Recipe.objects.all()
-        serializer = RecipeSerializer(recipes, many=True)
-        return JsonResponse(serializer.data, safe=False)
+class RecipeViewSet(mixins.ListModelMixin,
+                    mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    GenericViewSet):
 
-    if request.method == 'POST':
-        serializer = RecipeSerializer(data=request.data)
-        if serializer.is_valid():
-            recipe = serializer.save()
-            return JsonResponse(RecipeSerializer(recipe).data, status=201)
-        return JsonResponse(serializer.errors, status=404)
-
-
-@csrf_exempt
-def recipe_show(request, recipe_id):
-    try:
-        recipe = Recipe.objects.get(pk=recipe_id)
-        serializer = RecipeSerializer(recipe)
-        return JsonResponse(serializer.data, safe=False)
-    except ObjectDoesNotExist:
-        raise Http404('Recipe does not exists')
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    renderer_classes = [JSONRenderer]
