26cb58967521d53d9ee7c7ca6632b272df602d09
Matilda <matilda.smeds@gmail.com>
2020-02-11 12:26:50 +0100

    Failing test for SEARCH endpoint

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,13 +1,27 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
+from recipes.models import Recipe, Ingredient
+from django.core.exceptions import ObjectDoesNotExist
 
 import json
 
-RECIPE_SERIALIZATION = {
-    "id": 1,
-    "name": "Avocado Pasta",
-    "description": "Easy and fun",
-    "ingredients": [{"name": "Avocado"}, {"name": "Pasta"}],
+RECIPE_DICT = {
+    # Created with a data migration, would probably be better as a fixture
+    # or created inside the test
+    'pasta': {
+        "id": 1,
+        "name": "Avocado Pasta",
+        "description": "Easy and fun",
+        "ingredients": [{"name": "Avocado"}, {"name": "Pasta"}],
+    },
+    'pizza': {
+        "name": "Pizza",
+        "description": "Put it in the oven",
+        "ingredients": [
+            {"name": "dough"},
+            {"name": "cheese"},
+            {"name": "tomato"}],
+    }
 }
 
 
@@ -18,21 +32,14 @@ class RecipeAPITests(TestCase):
         self.client = APIClient()
         res = self.client.get(url, format="json")
 
-        expected = [RECIPE_SERIALIZATION]
+        expected = [RECIPE_DICT['pasta']]
         self.assertEquals(json.loads(res.content), expected)
         self.assertEquals(res.status_code, 200)
 
     def test_post_recipes(self):
         """Test POST /api/recipe"""
         url = "/api/recipe/"
-        params = {
-            "name": "Pizza",
-            "description": "Put it in the oven",
-            "ingredients": [
-                {"name": "dough"},
-                {"name": "cheese"},
-                {"name": "tomato"}],
-        }
+        params = RECIPE_DICT['pizza']
         res = self.client.post(
             url,
             json.dumps(params),
@@ -40,7 +47,8 @@ class RecipeAPITests(TestCase):
         )
 
         payload = json.loads(res.content)
-        params['id'] = payload['id']
+        id = payload.pop('id')
+        self.assertEquals(id, 2)
         self.assertEquals(payload, params)
         self.assertEquals(res.status_code, 201)
 
@@ -49,7 +57,16 @@ class RecipeAPITests(TestCase):
         url = "/api/recipe/1/"
         res = self.client.get(url, format="json")
 
-        expected = RECIPE_SERIALIZATION
+        expected = RECIPE_DICT['pasta']
+        self.assertEquals(json.loads(res.content), expected)
+        self.assertEquals(res.status_code, 200)
+
+    def test_search_recipe(self):
+        """Test GET /api/recipe/?name=<search string>"""
+        url = '/api/recipe/?name=Pi'
+        res = self.client.get(url, format='json')
+
+        expected = [RECIPE_DICT['pizza']]
         self.assertEquals(json.loads(res.content), expected)
         self.assertEquals(res.status_code, 200)
 
@@ -66,15 +83,26 @@ class RecipeAPITests(TestCase):
                                 content_type='application/json')
 
         payload = json.loads(res.content)
-        params['id'] = payload['id']
+        params['id'] = 1
 
         self.assertEquals(res.status_code, 200)
         self.assertEquals(payload, params)
+
+    def assert_object_does_not_exist(self, model, id):
+        with self.assertRaises(ObjectDoesNotExist):
+            model.objects.get(pk=id)
 
     # These tests can't be run in parallel atm
     def test_delete_recipe(self):
         """Test DELETE /api/recipe/:id"""
         url = "/api/recipe/1/"
+        recipe_id = 1
+        recipe = Recipe.objects.get(pk=recipe_id)
+        ingredient_ids = [ing.id for ing in recipe.ingredients.all()]
+
         res = self.client.delete(url)
 
         self.assertEquals(res.status_code, 204)
+        self.assert_object_does_not_exist(Recipe, 1)
+        for id in ingredient_ids:
+            self.assert_object_does_not_exist(Ingredient, id)
